78607397c02f95bab6a4aa99466a92a5ce2c8947
Evanilson <evanilsonmanoel85@gmail.com>
2025-03-31 16:54:00 -0300

    config api deploy

--- src/infra/http/repository/axios-course.repository.ts
+++ src/infra/http/repository/axios-course.repository.ts
@@ -5,9 +5,7 @@ import { MappersCourse } from './mappers/mappers.course';
 const URL = '/api/courses';
 export class AxiosCourseRepository implements CourseRepository {
   async getAllCourses(): Promise<Course[]> {
-    const { data } = await axiosRequest({ baseUrl: URL }).get(
-      'datastore_search?resource_id=738b884e-e846-4396-8cb3-f3390e00e598&limit=1000'
-    );
+    const { data } = await axiosRequest({ baseUrl: URL }).get('');
     return data.result.records.map(MappersCourse.toDomain);
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/courses': {
-        target: 'http://dados.recife.pe.gov.br/is/api/3/action/',
+        target:
+          'http://dados.recife.pe.gov.br/is/api/3/action/datastore_search?resource_id=738b884e-e846-4396-8cb3-f3390e00e598&limit=1000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/courses/, ''),
       },
